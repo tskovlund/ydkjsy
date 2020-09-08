9c8141edb50eb6cdc3393d8eb34a0fb910fd1fbe
Thomas Skovlund Hansen <tsk@it-minds.dk>
2020-09-08 13:46:13 +0200

    Add slotmachine WIP

---
+++ slotmachine.js
@@ -0,0 +1,55 @@
+function randMax(max) {
+  return Math.trunc(1e9 * Math.random()) % max;
+}
+
+var reel = {
+  symbols: ["♠", "♥", "♦", "♣", "☺", "★", "☾", "☀"],
+  spin() {
+    if (this.position == null) {
+      this.position = randMax(this.symbols.length - 1);
+    }
+    this.position = (this.position + 100 + randMax(100)) % this.symbols.length;
+  },
+  display() {
+    if (this.position == null) {
+      this.position = randMax(this.symbols.length - 1);
+    }
+    return this.symbols[this.position];
+  },
+};
+
+var slotMachine = {
+  reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
+  spin() {
+    this.reels.forEach(function spinReel(reel) {
+      reel.spin();
+    });
+  },
+  display() {
+    let line1 = "", line2 = "", line3 = "";
+    for (let reel of this.reels) {
+      // TODO: Fix modulo wrapping
+      reel.position -= 1;
+      line1 += reel.display() + " |";
+      reel.position += 1;
+      line2 += reel.display() + " |";
+      reel.position += 1;
+      line3 += reel.display() + " |";
+    }
+    console.log(line1);
+    console.log(line2);
+    console.log(line3);
+  },
+};
+
+slotMachine.spin();
+slotMachine.display();
+// ☾ | ☀ | ★
+// ☀ | ♠ | ☾
+// ♠ | ♥ | ☀
+
+slotMachine.spin();
+slotMachine.display();
+// ♦ | ♠ | ♣
+// ♣ | ♥ | ☺
+// ☺ | ♦ | ★
